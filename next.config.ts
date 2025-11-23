b71de502cea8d64a4259b24283e04c6d6c93bf21
Sultan <sultan.kurmanov@hardcode.kz>
2025-11-24 00:34:19 +0500

    nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   devIndicators: false,  // Полностью отключаем dev индикаторы
   experimental: {
     optimizePackageImports: ['@heroicons/react'],
